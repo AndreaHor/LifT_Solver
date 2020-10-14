44f52c37270cc5c3ab797d7aee2f8c0bf3112a13
Andrea <fuksova@lap-12-64.mpi-inf.mpg.de>
2020-10-14 13:17:42 +0200

    change input for twoGraphsInputStructure

--- solverILP/include/disjoint-paths/twoGraphsInputStructure.hxx
+++ solverILP/include/disjoint-paths/twoGraphsInputStructure.hxx
@@ -13,12 +13,12 @@ namespace disjointPaths {
 class TwoGraphsInputStructure
 {
 public:
-    TwoGraphsInputStructure(const py::array_t<size_t>& baseEdges,const py::array_t<size_t>& liftedEdges,const  py::array_t<double>& baseCosts,const  py::array_t<double>& liftedCosts,VertexGroups<>* pvg) {
+    TwoGraphsInputStructure(const py::array_t<size_t>& baseEdges,const py::array_t<size_t>& liftedEdges,const  py::array_t<double>& baseCosts,const  py::array_t<double>& liftedCosts,VertexGroups<>& pvg) {
         pBaseEdges=&baseEdges;
         pLiftedEdges=&liftedEdges;
         pBaseCosts=&baseCosts;
         pLiftedCosts=&liftedCosts;
-        myPvg=pvg;
+        myPvg=&pvg;
     }
     const py::array_t<size_t>* pBaseEdges;
     const py::array_t<size_t>* pLiftedEdges;
